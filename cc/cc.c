c61f3275f25fa3559dc7a960e127f230b9b1efa2
MalathiKumar <malathi.s@vvdntech.in>
2019-01-28 17:58:56 +0530

    fivth

---
+++ cc/cc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c;
+ printf("enter the two no.:");
+ scanf("%d%d",&a,&b);
+ c=a+b;
+ printf("the sum is %d",c);
+ c=a-b;
+ printf("the sub is %d",c);
+ c=a*b;
+ printf("the mult is %d",c);
+ c=a/b;
+ printf("the div is %d",c);
+ return 0;
+}
+
